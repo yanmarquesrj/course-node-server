21b264c7a22e7af3633a906829acef610f6f5bbe
Yan Marques <yanmarquesrj@gmail.com>
2018-10-27 16:13:38 -0300

    Secound commit?

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+//helper
+
+//Busca ano atual
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+//uppercase
+hbs.registerHelper('textUpperCase', (text) => {
+  return text.toUpperCase();
+});
+
+var app = express();
+app.set('view engine', 'hbs');
+
+//before reload page
+app.use( (req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method}, ${req.url} \n`;
+    console.log(log);
+    fs.appendFile('server.log', log, (err) => {
+      if(err){
+        console.log(err);
+      }
+    });
+  next();
+});
+
+
+// app.use( (req, res, next) => {
+//   res.render('manutencao.hbs');
+// });
+
+//static html
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    title: 'Home Page',
+    pageTitle: 'Home Title',
+    subTitle: 'home sub title'
+  })
+});
+
+app.get('/sobre', (req, res) => {
+    res.render('sobre.hbs', {
+      pageTitle: 'Sobre o Página'
+    });
+});
+
+app.listen(3000, () => {
+  console.log('Server connect in port 3000');
+});
